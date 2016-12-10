7ceadfa20e4942e0cd660c9556c931a7ff3979fa
Hayk Saribekyan <hayks@mit.edu>
2016-12-10 01:29:35 -0500

    triangle test for monotonicity

---
+++ test/check.h
@@ -0,0 +1,23 @@
+#include <printf.h>
+
+#include <vector>
+
+void check(std::vector< int > expected, std::vector< int > received, std::string name) {
+    if (expected == received) {
+        printf("PASS: %s\n", name.c_str());
+    } else {
+        printf("FAIL: %s\n", name.c_str());
+        printf("<<<< expected");
+        for (int i = 0; i < expected.size(); ++i) {
+            printf(" %d", expected[i]);
+        }
+        printf("\n");
+        printf(">>>> received");
+        for (int i = 0; i < received.size(); ++i) {
+            printf(" %d", received[i]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
--- test/test_naive.cpp
+++ test/test_naive.cpp
@@ -6,6 +6,7 @@
 #include "../src/helpers.h"
 #include "../src/naive.h"
 
+#include "check.h"
 
 void test_diam2d() {
     std::vector<Point> points = {{0.0, 0.0},
@@ -15,22 +16,29 @@ void test_diam2d() {
                                  {2.0, 2.0},
                                  {2.5, 2.5}};
 
-    std::vector<int> answer = naive_diameter(points);
     std::vector<int> exp_answer = {2, 2, 3, 3, 5, 5};
-    assert(answer == exp_answer);
-    printf("PASS: diam2d\n");
+    check(exp_answer, naive_diameter(points), "naive diam2d");
+}
+
+void test_monotonicity_simple_triangle() {
+    std::vector<Point> points = {{2.0, 0.0},
+                                 {0.0, 1.0},
+                                 {0.0, -1.0}};
+    std::vector<int> exp_answer = {2, 2, 2};
+
+    check(exp_answer, naive_monotonicity_2d(points), "naive monotonicity_simple_triangle");
 }
 
 void test_monotonicity_11gon() {
     std::vector<Point> data = load_csv_data("data/11-gon.csv");
-    std::vector<int> naive_answer = naive_monotonicity_2d(data);
     std::vector<int> exp_answer = {6, 7, 8, 9, 10, 10, 10, 10, 10, 10, 10};
-    assert(naive_answer == exp_answer);
-    printf("PASS: monotonicity_11gon\n");
+
+    check(exp_answer, naive_monotonicity_2d(data), "naive monotonicity_11gon");
 }
 
 int main() {
     test_diam2d();
+    test_monotonicity_simple_triangle();
     test_monotonicity_11gon();
     return 0;
 }
