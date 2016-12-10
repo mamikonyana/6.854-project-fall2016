79d4b29f5ed205068165d4bcf06b47a71d71e111
Arsen Mamikonyan <arsen@mamikonyan.am>
2016-12-10 18:35:31 -0500

    compiles

--- src/polyarc.cpp
+++ src/polyarc.cpp
@@ -2,24 +2,14 @@
 
 #include "polyarc.h"
 
-struct Point2D {
-    double x, y;
-};
+PolyArc PolyArc::intersect(PolyArc other) {
+    return PolyArc();
+}
 
-struct Vertex {
-    Point2D p;
-    Point2D center;
-    int c; // index of circle
-};
+PolyArc PolyArc::intersect(Point2D circle) {
+    return PolyArc();
+}
 
-class PolyArc {
-    private:
-        std::vector< std::Vertex > vertices;
-
-    public:
-        PolyArc intersect(PolyArc other);
-        PolyArc intersect(Point2D circle);
-        
-        static PolyArc fromCircles(Point2D c1, Point2D c2);
-};
-
+PolyArc PolyArc::fromCircles(Point2D c1, Point2D c2) {
+    return PolyArc();
+}
--- src/polyarc.h
+++ src/polyarc.h
@@ -8,20 +8,21 @@ struct Point2D {
 };
 
 struct Vertex {
-    Point2D p;
-    Point2D center;
-    int c; // index of circle
+    Point2D location;
+    Point2D arch_center;
+    int circle_index; // index of circle
 };
 
 class PolyArc {
-    private:
-        std::vector< std::Vertex > vertices;
+private:
+    std::vector<Vertex> vertices;
 
-    public:
-        PolyArc intersect(PolyArc other);
-        PolyArc intersect(Point2D circle);
-        
-        static PolyArc fromCircles(Point2D c1, Point2D c2);
+public:
+    PolyArc intersect(PolyArc other);
+
+    PolyArc intersect(Point2D circle);
+
+    static PolyArc fromCircles(Point2D c1, Point2D c2);
 };
 
 #endif // POLYARC_H
